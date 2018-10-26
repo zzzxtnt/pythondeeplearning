a8fd39749e4a174e03aed764c5309c46d465f79a
liyangdartan <liyangdartan@didichuxing.com>
2018-10-26 17:18:39 +0800

    路透社新闻数据

--- reuters.py
+++ reuters.py
@@ -1,8 +1,9 @@
-from keras.datasets import imdb
+from keras.datasets import reuters
 import numpy as np
 from keras import models
 from keras import layers
 import matplotlib.pyplot as plt
+from keras.utils.np_utils import to_categorical
 
 
 def vectorize_sequences(sequences, dimensiion=10000):
@@ -12,36 +13,34 @@ def vectorize_sequences(sequences, dimensiion=10000):
     return results
 
 
-(train_data, train_label), (test_data, test_label) = imdb.load_data(num_words=10000)
+(train_data, train_label), (test_data, test_label) = reuters.load_data(num_words=10000)
 
-word_index = imdb.get_word_index()
+word_index = reuters.get_word_index()
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
-# print(train_data[0])
-# print(train_label[0])
-# print(x_train[0])
+one_hot_train_labels = to_categorical(train_label)
+one_hot_test_labels = to_categorical(test_label)
 
-y_train = np.asarray(train_label).astype('float32')
-y_test = np.asarray(test_label).astype('float32')
+# y_train = np.asarray(train_label).astype('float32')
+# y_test = np.asarray(test_label).astype('float32')
 
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation="relu"))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation="relu"))
+model.add(layers.Dense(46, activation='softmax'))
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
 
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
 
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
-
-history = model.fit(partial_x_train, partial_y_train, epochs=4, batch_size=512, validation_data=(x_val, y_val))
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
 # print(history)
 
 history_dict = history.history
